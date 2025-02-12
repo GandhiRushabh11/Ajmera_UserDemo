bb2aa316737168228839c4020ddcc3b46bae3c3b
GandhiRushabh11 <gandhirushabh11@gmail.com>
2025-02-12 23:53:39 +0530

    Added if user already exits

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,16 @@ exports.createUser = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
 
+    // Check if user with email already exists
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return next(new AppError("Email already exists", 400));
+    }
+
     const user = await User.create({ name, email, password, role });
     res.status(201).json(user);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
