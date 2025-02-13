bef3442d8ab8d1c8bdbea210a3de45fe7fde354b
GandhiRushabh11 <gandhirushabh11@gmail.com>
2025-02-13 09:45:57 +0530

    added package.json

--- config/database.js
+++ config/database.js
@@ -1,19 +1,24 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
-    port: 3306,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
   }
 );
 sequelize
   .authenticate()
-  .then(() => console.log(`Database Connection established successfully.`))
-  .catch((err) => console.log(`Unable to connect to the database`, err));
-
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Database connection error:", err));
 module.exports = sequelize;
